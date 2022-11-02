2ce308eb3e5a258f81e0c124c4a7dad96bc16f92
AzureAD\엄지민 <eommji@bellins.net>
2022-11-02 11:00:55 +0900

    게시판 세팅(서버 제외)

---
+++ components/main/main.vue
@@ -0,0 +1,18 @@
+<template>
+<!-- 게시판 미리보기 부분 -->
+<div class="container" style="margin-top:100px">
+	<div class="row">
+    <main_board_list></main_board_list>
+    <main_board_list></main_board_list>
+    <main_board_list></main_board_list>
+    <main_board_list></main_board_list>
+	</div>
+</div>
+</template>
+<script>
+  module.exports = {
+    components: {
+      'main_board_list': httpVueLoader('components/main/main_board_list.vue')
+    }
+  }
+</script>
